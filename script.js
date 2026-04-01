49c006e8ff12e59e65f47f8770cdc3db42ee7e4e
KIRTI-GITHUB <33566404+KIRTIRAI-SLIET@users.noreply.github.com>
2026-04-01 22:24:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
 //your JS code here. If required.
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
 	const codes = document.querySelectorAll(".code");
 
-	// Focus first input initially
 	codes[0].focus();
 
 	codes.forEach((code, index) => {
 
-		// Handle typing forward
 		code.addEventListener("input", (e) => {
 			const value = e.target.value;
 
-			// Allow only digits
 			if (!/^[0-9]$/.test(value)) {
 				code.value = "";
 				return;
 			}
 
-			// Move to next input
 			if (index < codes.length - 1) {
 				codes[index + 1].focus();
 			}
 		});
 
-		// Handle backspace
 		code.addEventListener("keydown", (e) => {
 			if (e.key === "Backspace") {
 				if (code.value === "" && index > 0) {
